08baa82ee3fef00254b161a285889ad1ef5f4664
jjrdizon <jjrdizon@localhost>
2011-07-20 14:35:52 +0800

    added: UserSidebarPanel, UserSidebarPanelTest

---
+++ test/UserSidebarPanelTest.java
@@ -0,0 +1,37 @@
+package views;
+
+import static org.junit.Assert.*;
+
+import java.awt.Window;
+
+import org.junit.Test;
+
+public class UserSidebarPanelTest {
+	
+	@Test
+	public void testUserSidebarPanel() {
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddSearchBookListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddEditProfileListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddShowTransactionHistoryListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddLogoutListener() {
+		fail("Not yet implemented");
+	}
+
+}
