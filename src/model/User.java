7fb9cadf4080a6ada228d8746d064bf410188497
kanra.cpg <kanra.cpg@localhost>
2011-07-20 16:23:10 +0800

    Updated LogInDialog and added AuthenticationController

---
+++ src/model/User.java
@@ -0,0 +1,112 @@
+package model;
+
+public class User {
+	private int userId;
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String address;
+	private String contactNo;
+	private int status; 
+	private int type;
+	
+	public User(int userId, String userName, String password, String firstName,
+			String lastName, String email, String address, String contactNo,
+			int status, int type) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.address = address;
+		this.contactNo = contactNo;
+		this.status = status;
+		this.type = type;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	
+}
---
+++ test/UserSidebarPanelTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+
+import javax.swing.*;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.uispec4j.Button;
+import org.uispec4j.Panel;
+import org.unitils.UnitilsJUnit4TestClassRunner;
+
+import views.UserSidebarPanel;
+
+@RunWith(UnitilsJUnit4TestClassRunner.class)
+public class UserSidebarPanelTest {
+	
+	private Panel panel;
+	
+	@Before
+	public void setUp() throws Exception {
+		panel = new Panel(new UserSidebarPanel());
+	}
+	
+	@Test
+	public void initialState(){
+	}
+
+	@Test
+	public void testAddSearchBookListener() {
+		
+	}
+
+	@Test
+	public void testAddEditProfileListener() {
+		
+	}
+
+	@Test
+	public void testAddShowTransactionHistoryListener() {
+		
+	}
+
+	@Test
+	public void testAddLogoutListener() {
+		
+	}
+
+}
