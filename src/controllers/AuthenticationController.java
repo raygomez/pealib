a2aff3899b3e468ec62b3c9e19aa18dbac2ec2b6
rayvincent.gomez <rayvincent.gomez@localhost>
2011-07-21 14:12:30 +0800

    Added denyBookRequest method in TransactionDAO.

---
+++ src/controllers/AuthenticationController.java
@@ -0,0 +1,160 @@
+package controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JDialog;
+
+import models.User;
+import models.UserDAO;
+
+import utilities.Constants;
+import views.LogInDialog;
+
+public class AuthenticationController {
+	private static LogInDialog login; 
+	private User user;
+	private UserDAO userDao;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			new AuthenticationController();								
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public AuthenticationController(){
+		login = new LogInDialog();		
+		login.setActionListeners(new SignUpListener(), new SubmitListener(), new SubmitKeyListener());
+		login.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);					
+		login.setVisible(true);	
+		
+		try{
+			userDao = new UserDAO();
+					
+		} catch(Exception e){System.out.println("UserDAO "+e);}
+		
+		if(userDao == null){
+			System.out.println("123");
+		}
+	}
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	/*
+	 *  LOG IN   LISTENERS
+	 */
+	
+	//SignUp Listener
+	class SignUpListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+        	//open signup pane
+        }
+	}
+	
+	//Submit Listener
+	class SubmitListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+        	submit();
+        }
+	}
+	
+	//Submit Listener for Keyboard
+	class SubmitKeyListener implements KeyListener {
+		@Override
+		public void keyPressed(KeyEvent e) {			 
+		}
+		@Override
+		public void keyReleased(KeyEvent e) {
+			int keyCode = e.getKeyCode();
+			 
+			 if(keyCode==10){
+				 submit();
+			 } 
+			 else {
+				 String user = login.getFieldUsername().getText();
+				 String pass = login.getFieldPassword().getText();
+				 
+				 if(user.length()>0) validateUsername(user);
+				 if(pass.length()>0) validatePassword(pass); 				 
+			 }
+		}
+		@Override
+		public void keyTyped(KeyEvent e) {}
+	}
+	
+	private boolean validateUsername(String username){
+		boolean result = (username.matches(Constants.USERNAME_FORMAT));
+		
+		if(result) login.setUsernameColor(false);   
+		else  login.setUsernameColor(true);
+		
+		return result;
+	}
+	
+	private boolean validatePassword(String password){
+		boolean result = (password.matches(Constants.PASSWORD_FORMAT));
+		if(result) login.setPasswordColor(false);   
+		else  login.setPasswordColor(true);
+		
+		return result;
+	}
+	
+	private void submit(){
+		String username = login.getFieldUsername().getText();
+    	String password = login.getFieldPassword().getText();  	    	
+    	    	    	
+    	if(username.equals("") || password.equals("")){
+    		login.setLabelError("Incomplete fields");
+    		
+    		if (username.equals("")) login.setUsernameColor(true); 
+    		else login.setUsernameColor(false);
+    		
+    		if(password.equals("")) login.setPasswordColor(true);
+    		else login.setPasswordColor(false);
+    	}  
+    	else if(!validateUsername(username) || !validatePassword(password)){
+    		login.setLabelError("Invalid input");      		     		
+    	}
+    	
+    	else if(validateUsername(username) && validatePassword(password)){
+    		try{
+    			setUser(userDao.getUser(username, password));    
+    			System.out.println("username : "+user.getFirstName());
+    		} catch(Exception e){ System.out.println("AuthenticationController getUser: "+e);}    		    		
+    		
+    		
+    		if(user != null){
+	    		//login --> call main frame
+    			
+	    		System.out.println("LOGIN");
+	    		login.setUsernameColor(false);
+	    		login.setPasswordColor(false);
+	    		login.getFieldPassword().setText("");
+	    		login.getFieldUsername().setText("");
+	    		login.setLabelError("");
+	    		}
+    		else{
+    			login.setLabelError("Username/Password Mismatch");  
+        		login.getFieldPassword().setText("");
+    		}    		
+    	}
+    	
+	}	
+	
+	
+	
+}
---
+++ src/models/TransactionDAO.java
@@ -0,0 +1,99 @@
+package models;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Properties;
+
+import utilities.MyConnection;
+import utilities.PropertyLoader;
+
+public class TransactionDAO {
+
+	public final static int BOOK_UNAVAILABLE = 0;
+	public final static int SUCCESSFUL = 1;
+	public final static int ALREADY_IN = 2;
+	private Connection connection;
+
+	public TransactionDAO() throws SQLException, ClassNotFoundException {
+		Properties properties = new PropertyLoader("app.config")
+				.getProperties();
+		connection = new MyConnection(properties).getConnection();
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
+	public void receiveBook(Borrow borrow) throws SQLException {
+
+		String sql = "UPDATE Borrows SET ReturnDate = ? where ID = ?";
+		Calendar today = Calendar.getInstance();
+
+		PreparedStatement ps = getConnection().prepareStatement(sql);
+		ps.setDate(1, new Date(today.getTime().getTime()));
+		ps.setInt(2, borrow.getId());
+		ps.executeUpdate();
+
+	}
+
+	public int reserveBook(Book book, User user) throws SQLException {
+		int intStat = 0;
+		PreparedStatement makeReservation = null;
+
+		makeReservation = connection
+				.prepareStatement("INSERT INTO Reserves (UserID, BookID) VALUES (?,?)");
+		makeReservation.setLong(1, user.getUserId());
+		makeReservation.setLong(2, book.getBookId());
+
+		intStat = makeReservation.executeUpdate();
+
+		return intStat;
+	}
+
+	public int borrowBook(Book book, User user) throws SQLException {
+		int intStat = 0;
+		PreparedStatement borrowRequest = null;
+
+		borrowRequest = connection
+				.prepareStatement("INSERT INTO Borrows (UserID, BookID) VALUES (?,?)");
+		borrowRequest.setLong(1, user.getUserId());
+		borrowRequest.setLong(2, book.getBookId());
+
+		intStat = borrowRequest.executeUpdate();
+
+		return intStat;
+	}
+
+	public int acceptBookRequest(Borrow borrowedBook) throws SQLException {
+		Calendar today = Calendar.getInstance();
+		int intStat = 0;
+		PreparedStatement acceptRequest = null;
+
+		acceptRequest = connection
+				.prepareStatement("UPDATE Borrows SET DateBorrowed = ? where BorrowID = ?");
+		acceptRequest.setDate(1, new Date(today.getTime().getTime()));
+		acceptRequest.setLong(2, borrowedBook.getId());
+
+		intStat = acceptRequest.executeUpdate();
+
+		return intStat;
+	}
+
+	public void denyBookRequest(Borrow borrow) throws SQLException {
+
+		String sql = "Delete from Borrows where ID = ?";
+
+		PreparedStatement ps = getConnection().prepareStatement(sql);
+		ps.setInt(1, borrow.getId());
+		ps.executeUpdate();
+
+	}
+
+}
