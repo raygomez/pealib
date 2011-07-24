33684bc9f076c78e1d4c62105cc60268d4943366
rayvincent.gomez <rayvincent.gomez@localhost>
2011-07-25 07:27:04 +0800

    Updated cobertura tests in pealibbuild. Setup test for UserController.

--- test/controllers/UserControllerTest.java
+++ test/controllers/UserControllerTest.java
@@ -1,21 +1,40 @@
 package controllers;
 
-import javax.swing.JPanel;
+import models.User;
+import models.UserDAO;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.uispec4j.Panel;
 import org.uispec4j.UISpecTestCase;
+import org.unitils.UnitilsJUnit4TestClassRunner;
+import org.unitils.dbunit.annotation.DataSet;
 
+import utilities.Connector;
+import utilities.Constants;
+
+@DataSet({ "../models/user.xml" })
+@RunWith(UnitilsJUnit4TestClassRunner.class)
 public class UserControllerTest extends UISpecTestCase {
 
+	Panel userInfoPanel;
+	Panel userSearchPanel;
+	
 	@Before
 	public void setUp() throws Exception {
+		new Connector(Constants.TEST_CONFIG);
+		
+		User user = UserDAO.getUserById(2);
+		userInfoPanel = new Panel(new UserController(user).getUserInfoPanel());
+		assertNotNull(userInfoPanel);
+		userSearchPanel = new Panel(new UserController(user).getUserSearch());
+		assertNotNull(userSearchPanel);
 	}
 
 	@Test
 	public void testGetUserPanel() {
-		JPanel panel = new UserController(null).getUserPanel();
-		assertNotNull(panel);
+
 	}
 
 }
