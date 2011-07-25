43c5c62c11b5cab94592a72e24af45d8101eef04
nielorockz <nielorockz@localhost>
2011-07-25 10:23:00 +0800

    committed latest version of BookController, BookInfoPanel, BookSearchPanel

--- src/controllers/BookController.java
+++ src/controllers/BookController.java
@@ -4,10 +4,10 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -34,26 +34,25 @@ public class BookController {
 	private User currentUser;
 	private ArrayList<Book> bookList;
 
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) throws Exception{
 		new Connector(Constants.TEST_CONFIG);
-		User user = new User(19, "niel", "121111", "Reiniel Adam", "Lozada",
-				"reiniel_lozada@yahoo.com", "secret", "8194000", 1, "User");
+		User user = new User(19, "niel", "121111", "Reiniel Adam", "Lozada", "reiniel_lozada@yahoo.com", "secret", "8194000", 1, "User");
 		BookController bookController = new BookController(user);
 		JFrame testFrame = new JFrame();
 		testFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		testFrame.setVisible(true);
 		testFrame.setResizable(false);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		testFrame.setBounds(0, 0, screenSize.width, screenSize.height);
+		testFrame.setBounds(0,0,screenSize.width, screenSize.height);
 		testFrame.setContentPane(bookController.getBookLayoutPanel());
 	}
-
-	public BookController(User user) throws Exception {
+	
+	public BookController(User user) throws Exception{
 		currentUser = user;
 		generateBookLayoutPanel();
 	}
-
-	private void generateBookLayoutPanel() throws Exception {
+	
+	private void generateBookLayoutPanel() throws Exception{
 		bookList = BookDAO.searchBook("*");
 		currSearchString = "*";
 		bookLayoutPanel = new JPanel(new MigLayout("wrap 2", "[grow][grow]"));
@@ -62,22 +61,23 @@ public class BookController {
 		currTableRowSelection = 0;
 		bookLayoutPanel.add(bookSearch, "grow");
 		bookLayoutPanel.add(bookInfo, "grow");
-
-		bookSearch.setTextFieldListener(new TextFieldAdapter());
+		
+		bookSearch.setTextFieldListener(new TextFieldListener());
 		bookSearch.setClearButtonListener(new ClearButtonListener());
 		bookSearch.setSearchButtonListener(new SearchButtonListener());
-		bookSearch
-				.setTableListModel(new BookListModel(BookDAO.searchBook("*")));
-		bookSearch.setMouseListener(new BookListMouseAdapter());
+		bookSearch.setTableListModel(new BookListModel(BookDAO.searchBook("*")));
+		bookSearch.setMouseListener(new BookListMouseListener());
 		bookInfo.addSaveListener(new SaveButtonListener());
 		bookInfo.addDeleteListener(new DeleteButtonListener());
 	}
-
+	
 	public JPanel getBookLayoutPanel() {
 		return bookLayoutPanel;
 	}
-
-	class SaveButtonListener implements ActionListener {
+	
+	
+	
+	class SaveButtonListener implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -90,9 +90,11 @@ public class BookController {
 				e1.printStackTrace();
 			}
 		}
+		
 	}
-
-	class DeleteButtonListener implements ActionListener {
+	
+	
+	class DeleteButtonListener implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -105,10 +107,11 @@ public class BookController {
 				e1.printStackTrace();
 			}
 		}
-
+		
 	}
+	
+	class BookListMouseListener implements MouseListener{
 
-	class BookListMouseAdapter extends MouseAdapter {
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			JTable table = (JTable) e.getSource();
@@ -117,45 +120,61 @@ public class BookController {
 			bookInfo.setBookInfoData(bookList.get(tableRow));
 		}
 
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {	
+		}
 	}
 
-	class BookListModel extends AbstractTableModel {
+	class BookListModel extends AbstractTableModel{		
 		/**
 		 * 
 		 */
 		private static final long serialVersionUID = 1L;
-		String[] columnName = { "ISBN No.", "Book", "Status" };
+		ArrayList<String> columnName = new ArrayList<String>();
 		ArrayList<ArrayList<String>> tableData = new ArrayList<ArrayList<String>>();
-
+		
 		public BookListModel(ArrayList<Book> bookList) {
 			ArrayList<String> rowData = new ArrayList<String>();
-
-			for (int i = 0; i < bookList.size(); i++) {
+			columnName.add("ISBN No.");
+			columnName.add("Book");
+			columnName.add("Status");
+			
+			for(int i = 0; i < bookList.size(); i++){
 				rowData = new ArrayList<String>();
 				rowData.add(bookList.get(i).getIsbn());
-				rowData.add(bookList.get(i).getTitle() + ", "
-						+ bookList.get(i).getAuthor());
-
-				if (bookList.get(i).getCopies() == 0) {
+				rowData.add(bookList.get(i).getTitle()+", "+bookList.get(i).getAuthor());
+				
+				if(bookList.get(i).getCopies() == 0){
 					rowData.add("unavailable");
-				} else if (bookList.get(i).getCopies() == 1) {
+				}else if(bookList.get(i).getCopies() == 1){
 					rowData.add("1 copy available");
-				} else {
-					rowData.add(bookList.get(i).getCopies()
-							+ " copies available");
+				}else{
+					rowData.add(bookList.get(i).getCopies()+" copies available");
 				}
-
+				
 				tableData.add(rowData);
 			}
 		}
 
 		public String getColumnName(int col) {
-			return columnName[col];
-		}
-
+	         return columnName.get(col);
+	     }
+		
 		@Override
 		public int getColumnCount() {
-			return columnName.length;
+			return columnName.size();
 		}
 
 		@Override
@@ -167,20 +186,20 @@ public class BookController {
 		public Object getValueAt(int rowIndex, int columnIndex) {
 			return tableData.get(rowIndex).get(columnIndex);
 		}
-
+		
 		@Override
-		public boolean isCellEditable(int rowIndex, int columnIndex) {
+		public boolean isCellEditable(int rowIndex, int columnIndex){
 			return false;
 		}
-
+		
 	}
-
-	class SearchButtonListener implements ActionListener {
+	
+	class SearchButtonListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			try {
 				String strSearch = bookSearch.getTextFieldSearch();
-				if (!strSearch.trim().isEmpty()) {
+				if(!strSearch.trim().isEmpty()){
 					currSearchString = strSearch;
 					bookList = BookDAO.searchBook(strSearch);
 					bookSearch.setTableListModel(new BookListModel(bookList));
@@ -190,36 +209,45 @@ public class BookController {
 				ex.printStackTrace();
 			}
 		}
-
+		
 	}
-
-	class ClearButtonListener implements ActionListener {
+	
+	class ClearButtonListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			bookSearch.setTextFieldSearch("");
 		}
-
+		
 	}
+	
+	class TextFieldListener implements KeyListener{
 
-	class TextFieldAdapter extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent key) {
+		}
+
 		@Override
 		public void keyReleased(KeyEvent key) {
-			if (key.getKeyCode() == KeyEvent.VK_ENTER) {
+			if(key.getKeyCode() == KeyEvent.VK_ENTER){
 				try {
 					String strSearch = bookSearch.getTextFieldSearch();
-					if (!strSearch.trim().isEmpty()) {
+					if(!strSearch.trim().isEmpty()){
 						currSearchString = strSearch;
 						bookList = BookDAO.searchBook(strSearch);
-						bookSearch
-								.setTableListModel(new BookListModel(bookList));
+						bookSearch.setTableListModel(new BookListModel(bookList));
 						bookInfo.setBookInfoData(bookList.get(0));
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-
+			
 		}
 
+		@Override
+		public void keyTyped(KeyEvent key) {			
+		}
+		
 	}
+	
 }
