8192c4c94d5bda9e01f14aa6729c2182228c5c56
jomel.villar <jomel.villar@localhost>
2011-07-25 15:02:57 +0800

    Added implementation for Librarian Book Transactions.

---
+++ src/controllers/TransactionController.java
@@ -0,0 +1,360 @@
+package controllers;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import models.BorrowTransaction;
+import models.TransactionDAO;
+
+import views.InOutTabbedPane;
+import views.IncomingPanel;
+import views.OutgoingPanel;
+//# remove these
+import utilities.Connector;
+import utilities.Constants;
+
+
+public class TransactionController {
+	private boolean isIncoming = true; //isOutgoing = false
+	private TableModel bookResultsModel;
+	private String[] tableHeader;
+	private String[] incomingTableHeader = {"ISBN", "Title", "Author", "Username", "Date Borrowed"};
+	private String[] outgoingTableHeader = {"ISBN", "Title", "Author", "Username", "Date Requested"};
+	private ArrayList<BorrowTransaction> searchResults;
+	private int selectedRow; 
+	
+	private static InOutTabbedPane tabbedPane;
+	private IncomingPanel inPanel;
+	private OutgoingPanel outPanel;
+
+
+	public static void main(String[] args) {
+		//# remove this
+		new Connector(Constants.TEST_CONFIG);
+		JFrame testFrame = new JFrame();
+		testFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		testFrame.setVisible(true);
+		testFrame.setResizable(false);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		testFrame.setBounds(0,0,screenSize.width, screenSize.height * 3/4);
+		
+		tabbedPane = new InOutTabbedPane();
+		testFrame.setContentPane(tabbedPane);
+		
+		try {
+			new TransactionController();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * LIBRARIAN Book Transactions
+	 */
+	public TransactionController() throws Exception {
+		/* Load Event Listeners */
+		/* InOutTabbedPane */
+		tabbedPane.setEventListener(new TabChangeListener());
+		/* OutgoingPanel */
+		outPanel = tabbedPane.getOutgoingPanel();
+		outPanel.setEventListener(new OutgoingBooksGrantListener(), new OutgoingBooksDenyListener());
+		/* IncomingPanel */
+		inPanel = tabbedPane.getIncomingPanel();
+		inPanel.setEventListener(new IncomingBooksReturnListener());
+		/* InOutBookSearchPanel */
+		outPanel.getSearchPanel().setEventListeners(new BookTransactionSearchSubmitListener(), 
+				new BookTransactionSearchClearListener(), new BookTransactionSearchKeyListener(),
+				new BookTransactionResultsMouseListener());
+		inPanel.getSearchPanel().setEventListeners(new BookTransactionSearchSubmitListener(), 
+				new BookTransactionSearchClearListener(), new BookTransactionSearchKeyListener(),
+				new BookTransactionResultsMouseListener());
+		
+		/* default operation for Incoming */
+		searchBookTransaction();
+	}
+	
+	class TabChangeListener implements ChangeListener {
+		@Override
+		public void stateChanged(ChangeEvent arg0) {
+			if (tabbedPane.getSelectedIndex() == 0) {
+				isIncoming = true;
+			} else if (tabbedPane.getSelectedIndex() == 1) {
+				isIncoming = false;
+			} else { //-1
+				//# error
+				System.out.println(tabbedPane.getSelectedIndex());
+			}
+			searchBookTransaction();
+		}
+	}
+	
+	/*
+	 * Outgoing Transactions
+	 */
+	class OutgoingBooksGrantListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			grantBorrowRequest();
+			searchBookTransaction();
+		}
+	}
+	
+	class OutgoingBooksDenyListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			denyBorrowRequest();
+			searchBookTransaction();
+		}
+	}
+	
+	private void grantBorrowRequest() {
+		int affectedRows = 0;
+		try {
+			affectedRows = TransactionDAO.borrowBook(getBookTransactionDetails());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (affectedRows != 1) {
+			//# error! do something
+		}
+	}
+	
+	private void denyBorrowRequest() {
+		try {
+			TransactionDAO.denyBookRequest(getBookTransactionDetails());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * Incoming Transactions
+	 */
+	class IncomingBooksReturnListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			returnBook();
+			searchBookTransaction();
+		}
+	}
+	
+	private void returnBook() {
+		//# check if reserved by other users
+		try {
+			TransactionDAO.returnBook(getBookTransactionDetails());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Return: " + getBookTransactionDetails().getBook().getTitle());
+	}
+	
+	private BorrowTransaction getBookTransactionDetails() {
+		return searchResults.get(selectedRow);
+	}
+	
+	/*
+	 * Book Search and Results Table (for Borrowed and Pending books)
+	 */
+	class BookTransactionSearchSubmitListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			searchBookTransaction();
+		}
+	}
+	
+	class BookTransactionSearchClearListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (isIncoming) {
+				inPanel.getSearchPanel().getTxtfldSearch().setText("");
+			} else {
+				outPanel.getSearchPanel().getTxtfldSearch().setText("");
+			}
+		}
+	}
+	
+	class BookTransactionSearchKeyListener implements KeyListener {
+		@Override
+		public void keyPressed(KeyEvent e) {
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			final int enterKey = 10; 
+			int userKey = e.getKeyCode();
+	
+			if (userKey == enterKey) {
+				searchBookTransaction();
+			}
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+		}
+	}
+	
+	class BookTransactionResultsMouseListener implements MouseListener {
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			setSelectedItem();
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+		}
+	}
+	
+	private void searchBookTransaction() {
+		try {
+			String keyword;
+			if (isIncoming) {
+				keyword = inPanel.getSearchPanel().getTxtfldSearch().getText();
+				if (keyword.isEmpty()) {
+					keyword = "*";
+				}
+				searchResults = TransactionDAO.searchIncomingBook(keyword);
+				tableHeader = incomingTableHeader;
+			} else {
+				keyword = outPanel.getSearchPanel().getTxtfldSearch().getText();
+				if (keyword.isEmpty()) {
+					keyword = "*";
+				}
+				searchResults = TransactionDAO.searchOutgoingBook(keyword);
+				tableHeader = outgoingTableHeader;
+			}
+			
+			if (searchResults.size() != 0) {
+				bookResultsModel = new AbstractTableModel() {
+					private static final long serialVersionUID = 1L;
+					@Override
+					public int getColumnCount() {
+						return tableHeader.length;
+					}
+					
+					public String getColumnName(int column) {
+							return tableHeader[column];
+					}
+
+					@Override
+					public int getRowCount() {
+						return searchResults.size();
+					}
+
+					public boolean isCellEditable(int row, int column) {
+						return false;
+					}
+					
+					@Override
+					public Object getValueAt(int row, int column) {
+						Object objData = null;
+						switch (column) {
+						case 0:
+							objData = searchResults.get(row).getBook().getIsbn();
+							break;
+						case 1:
+							objData = searchResults.get(row).getBook().getTitle();
+							break;
+						case 2:
+							objData = searchResults.get(row).getBook().getAuthor();
+							break;
+						case 3:
+							objData = searchResults.get(row).getUser().getUserName();
+							break;
+						case 4:
+							if (isIncoming) {
+								objData = searchResults.get(row).getDateBorrowed();
+							} else {
+								objData = searchResults.get(row).getDateRequested();
+							}
+							break;
+						default:
+							break;
+						}
+						return objData;
+					}
+				};
+			} else {
+				String[][] emptyTable = {};
+				bookResultsModel = new DefaultTableModel(emptyTable, tableHeader) {
+					private static final long serialVersionUID = 1L;
+					public boolean isCellEditable(int row, int column) {
+						return false;
+					}
+				};
+			}
+			if (isIncoming) {
+				inPanel.getSearchPanel().getTblResults().setModel(bookResultsModel);
+				inPanel.getSearchPanel().getLblTotal().setText("Total Matches: " + bookResultsModel.getRowCount());
+				inPanel.getSearchPanel().getTblResults().getColumnModel().getColumn(0).setPreferredWidth(20);
+				inPanel.getSearchPanel().getTblResults().getColumnModel().getColumn(1).setPreferredWidth(200);
+				inPanel.getSearchPanel().getTblResults().getColumnModel().getColumn(2).setPreferredWidth(70);
+				inPanel.getSearchPanel().getTblResults().getColumnModel().getColumn(3).setPreferredWidth(30);
+				inPanel.getSearchPanel().getTblResults().getColumnModel().getColumn(4).setPreferredWidth(20);
+				
+				inPanel.getSearchPanel().repaint();
+			} else {
+				outPanel.getSearchPanel().getTblResults().setModel(bookResultsModel);
+				outPanel.getSearchPanel().getLblTotal().setText("Total Matches: " + bookResultsModel.getRowCount());
+				outPanel.getSearchPanel().getTblResults().getColumnModel().getColumn(0).setPreferredWidth(20);
+				outPanel.getSearchPanel().getTblResults().getColumnModel().getColumn(1).setPreferredWidth(200);
+				outPanel.getSearchPanel().getTblResults().getColumnModel().getColumn(2).setPreferredWidth(70);
+				outPanel.getSearchPanel().getTblResults().getColumnModel().getColumn(3).setPreferredWidth(30);
+				outPanel.getSearchPanel().getTblResults().getColumnModel().getColumn(4).setPreferredWidth(20);
+				outPanel.getSearchPanel().repaint();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void setSelectedItem() {
+		//# conflict with buttons and label messages
+		if (isIncoming) {
+			selectedRow = inPanel.getSearchPanel().getTblResults().getSelectedRow();
+			
+			if (selectedRow >= 0) {
+				inPanel.getBtnReturn().setEnabled(true);
+				getBookTransactionDetails();
+				try {
+					int daysOverdue = TransactionDAO.getDaysOverdue(getBookTransactionDetails());
+					inPanel.getLblDaysOverdue().setText(daysOverdue + " days Overdue.");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} else {
+			selectedRow = outPanel.getSearchPanel().getTblResults().getSelectedRow();
+			
+			if (selectedRow >= 0) {
+				outPanel.getBtnGrant().setEnabled(true);
+				outPanel.getBtnDeny().setEnabled(true);
+			}
+		}
+	}
+}
--- src/views/InOutBookSearchPanel.java
+++ src/views/InOutBookSearchPanel.java
@@ -2,16 +2,16 @@ package views;
 
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
-import net.miginfocom.swing.MigLayout;
-
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.table.TableModel;
+import net.miginfocom.swing.MigLayout;
+
 
 public class InOutBookSearchPanel extends JPanel {
 
@@ -22,12 +22,11 @@ public class InOutBookSearchPanel extends JPanel {
 	private JLabel lblSearch;
 	private JTable tblResults;
 	private JLabel lblTotal;
-	private TableModel transactionTable;
 	/**
 	 * Create the panel.
 	 */
 	public InOutBookSearchPanel() {
-		setLayout(new MigLayout("", "[55:n:55][230:n:230][100:n:100][100:n:100]", "[][25:n:25][15:n:15][25:n:25][280:n:280]"));
+		setLayout(new MigLayout("", "[55:n:55][500:n:500][100:n:100][100:n:100]", "[][25:n:25][15:n:15][25:n:25][280:n:280]"));
 		
 		lblSearch = new JLabel("Search");
 		add(lblSearch, "cell 0 0,alignx right");
@@ -46,32 +45,29 @@ public class InOutBookSearchPanel extends JPanel {
 		lblTotal = new JLabel("");
 		add(lblTotal, "cell 0 2 2 1");
 		
-		tblResults = new JTable(transactionTable);
+		tblResults = new JTable();
 		JScrollPane sclpnResults = new JScrollPane(tblResults);
 		add(sclpnResults, "cell 0 3 4 2,grow");
 	}
 	
-	/* Listeners */
-	public void setActionListeners(ActionListener submit, ActionListener clear, KeyListener enter) {
+	/* Event Listeners */
+	public void setEventListeners(ActionListener submit, ActionListener clear, KeyListener enter, MouseListener select) {
 		btnSubmit.addActionListener(submit);
 		btnClear.addActionListener(clear);
 		txtfldSearch.addKeyListener(enter);
+		tblResults.addMouseListener(select);
 	}
 	
-	/* Getters and Setters */
-	public TableModel getTransactionTable() {
-		return transactionTable;
-	}
-	
-	public void setTransactionTable(TableModel transactionTable) {
-		this.transactionTable = transactionTable;
-	}
-
+	/* Getters */
 	public JTextField getTxtfldSearch() {
 		return txtfldSearch;
 	}
 
 	public JLabel getLblTotal() {
 		return lblTotal;
+	}
+
+	public JTable getTblResults() {
+		return tblResults;
 	}
 }
