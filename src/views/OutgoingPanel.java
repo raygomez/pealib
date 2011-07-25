a006ab5bd716ff62320fa92ea59b60cff171b453
jomel.villar <jomel.villar@localhost>
2011-07-25 15:02:57 +0800

    Added implementation for Librarian Book Transactions.

--- src/views/OutgoingPanel.java
+++ src/views/OutgoingPanel.java
@@ -1,23 +1,49 @@
 package views;
 
+import java.awt.event.ActionListener;
+
 import javax.swing.JPanel;
-import net.miginfocom.swing.MigLayout;
 import javax.swing.JButton;
+
+import net.miginfocom.swing.MigLayout;
+
 
 public class OutgoingPanel extends JPanel{
 
 	private static final long serialVersionUID = 1L;
-
+	private InOutBookSearchPanel searchPanel;
+	private JButton btnGrant;
+	private JButton btnDeny;
+	
 	public OutgoingPanel() {
-		setLayout(new MigLayout("", "[25:n:25][266:n:266][100:n:100][100:n:100]", "[65:n:65][325:n:325][25:n:25]"));
+		setLayout(new MigLayout("", "[25:n:25][537:n:537][100:n:100][100:n:100]", "[65:n:65][325:n:325][25:n:25]"));
 		
-		JPanel searchPanel = new InOutBookSearchPanel();
+		searchPanel = new InOutBookSearchPanel();
 		add(searchPanel, "cell 0 0 4 2,grow");
 		
-		JButton btnGrant = new JButton("Grant");
+		btnGrant = new JButton("Grant");
 		add(btnGrant, "cell 2 2,growx");
 		
-		JButton btnDeny = new JButton("Deny");
+		btnDeny = new JButton("Deny");
 		add(btnDeny, "cell 3 2,growx");
+	}
+
+	/* Event Listeners */
+	public void setEventListener(ActionListener grant, ActionListener deny) {
+		btnGrant.addActionListener(grant);
+		btnDeny.addActionListener(deny);
+	}
+		
+	/* Getters */
+	public InOutBookSearchPanel getSearchPanel() {
+		return searchPanel;
+	}
+
+	public JButton getBtnGrant() {
+		return btnGrant;
+	}
+
+	public JButton getBtnDeny() {
+		return btnDeny;
 	}
 }
