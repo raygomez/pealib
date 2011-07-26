ff132dddc5839c4dd0251036176b17b14371d5b6
jomel.villar <jomel.villar@localhost>
2011-07-26 16:23:56 +0800

    Updated InOutBookSearchPanelTest. Clear button was removed.

---
+++ src/models/User.java
@@ -0,0 +1,118 @@
+package models;
+
+public class User {
+	private int userId;
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String address;
+	private String contactNo;
+	private String type;
+	
+	public User(){
+		
+	}
+	
+	public User(int userId, String userName, String password, String firstName,
+			String lastName, String email, String address, String contactNo, String type) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.address = address;
+		this.contactNo = contactNo;
+		this.type = type;
+	}
+	
+	public User(int userId, String userName, String firstName,
+			String lastName, String email, String address, String contactNo, String type) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.address = address;
+		this.contactNo = contactNo;
+		this.type = type;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
---
+++ src/views/ChangePasswordDialog.java
@@ -0,0 +1,187 @@
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import net.miginfocom.swing.MigLayout;
+import utilities.Constants;
+import utilities.ErrorLabel;
+
+	@SuppressWarnings("serial")
+	public class ChangePasswordDialog extends JDialog {
+
+		private final JPanel contentPanel = new JPanel();
+		private JPasswordField oldPasswordField;
+		private JPasswordField newPasswordField;
+		private JPasswordField repeatPasswordField;
+		private JButton changePasswordButton;
+		private JLabel errorLabel;
+		private JLabel oldPasswordLabel;
+
+		/**
+		 * Create the dialog.
+		 */
+		public ChangePasswordDialog() {
+			setModal(true);
+			createGUI();
+		}
+
+		public void createGUI() {
+
+			getContentPane().setLayout(new BorderLayout());
+			getContentPanel().setBorder(new EmptyBorder(5, 5, 5, 5));
+			getContentPane().add(getContentPanel(), BorderLayout.CENTER);
+			getContentPanel().setLayout(new MigLayout("", "[96.00][grow]",
+					"[][][][]"));
+
+			setOldPasswordLabel(new JLabel("Old Password:"));
+			getContentPanel().add(getOldPasswordLabel(), "cell 0 0,alignx trailing");
+			setOldPasswordField(new JPasswordField());
+			getOldPasswordField().setName("oldpassword");
+			getContentPanel().add(getOldPasswordField(), "cell 1 0,growx");
+
+			JLabel lblNewLabel_1 = new JLabel("New Password:");
+			getContentPanel().add(lblNewLabel_1, "cell 0 1,alignx trailing");
+			setNewPasswordField(new JPasswordField());
+			getNewPasswordField().setName("newpassword");
+			getContentPanel().add(getNewPasswordField(), "cell 1 1,growx");
+
+			JLabel lblNewLabel_2 = new JLabel("Repeat Password:");
+			lblNewLabel_2.setHorizontalAlignment(SwingConstants.LEFT);
+			getContentPanel().add(lblNewLabel_2, "cell 0 2,alignx trailing");
+			setRepeatPasswordField(new JPasswordField());
+			getRepeatPasswordField().setName("repeatpassword");
+			getContentPanel().add(getRepeatPasswordField(), "cell 1 2,growx");
+
+			setErrorLabel(new ErrorLabel(" "));
+			getContentPanel().add(getErrorLabel(), "cell 0 3 2 1");
+
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+
+			setChangePasswordButton(new JButton("Change Password"));
+			buttonPane.add(getChangePasswordButton());
+			getRootPane().setDefaultButton(getChangePasswordButton());
+
+			JButton cancelButton = new JButton("Cancel");
+			cancelButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					dispose();
+				}
+			});
+			buttonPane.add(cancelButton);
+			setLocationRelativeTo(null);
+			pack();
+		}
+
+		public JPasswordField getOldPasswordField() {
+			return oldPasswordField;
+		}
+
+		public void setOldPasswordField(JPasswordField oldPasswordField) {
+			this.oldPasswordField = oldPasswordField;
+		}
+
+		public JPasswordField getNewPasswordField() {
+			return newPasswordField;
+		}
+
+		public void setNewPasswordField(JPasswordField newPasswordField) {
+			this.newPasswordField = newPasswordField;
+		}
+
+		public JPasswordField getRepeatPasswordField() {
+			return repeatPasswordField;
+		}
+
+		public void setRepeatPasswordField(JPasswordField repeatPasswordField) {
+			this.repeatPasswordField = repeatPasswordField;
+		}
+
+		/**
+		 * @return the changePasswordButton_
+		 */
+		public JButton getChangePasswordButton() {
+			return changePasswordButton;
+		}
+
+		/**
+		 * @param changePasswordButton
+		 *            the changePasswordButton to set
+		 */
+		public void setChangePasswordButton(JButton changePasswordButton) {
+			this.changePasswordButton = changePasswordButton;
+		}
+
+		/**
+		 * @return the errorLabel
+		 */
+		public JLabel getErrorLabel() {
+			return errorLabel;
+		}
+
+		/**
+		 * @param errorLabel
+		 *            the errorLabel to set
+		 */
+		public void setErrorLabel(JLabel errorLabel) {
+			this.errorLabel = errorLabel;
+		}
+
+		public void addChangePasswordListener(ActionListener actionListener) {
+			getChangePasswordButton().addActionListener(actionListener);
+		}
+
+		/**
+		 * @return the contentPanel
+		 */
+		public JPanel getContentPanel() {
+			return contentPanel;
+		}
+		
+		/**
+		 * @return the oldPasswordLabel
+		 */
+		public JLabel getOldPasswordLabel() {
+			return oldPasswordLabel;
+		}
+
+		/**
+		 * @param oldPasswordLabel the oldPasswordLabel to set
+		 */
+		public void setOldPasswordLabel(JLabel oldPasswordLabel) {
+			this.oldPasswordLabel = oldPasswordLabel;
+		}
+
+		public void removeOldPassword(){
+			getOldPasswordField().setVisible(false);
+			getOldPasswordLabel().setVisible(false);			
+			getOldPasswordField().setEnabled(false);
+		}				
+
+		public void displayError(int error) {
+			// TODO Auto-generated method stub
+			switch (error){
+				case Constants.DEFAULT_ERROR:
+					errorLabel.setText("An error has occured");
+					break;
+				case Constants.INCORRECT_PASSWORD_ERROR:
+					errorLabel.setText("Incorrect password");
+					break;
+				case Constants.PASSWORD_NOT_MATCH_ERROR:
+					errorLabel.setText("New passwords do not match");
+					break;
+			}
+		}
+	}
