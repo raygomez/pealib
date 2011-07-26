08ee8f22fe60437592c208ce07491d078a6978ef
nielorockz <nielorockz@localhost>
2011-07-26 14:05:01 +0800

    added edition input box

--- src/views/AddBookDialog.java
+++ src/views/AddBookDialog.java
@@ -35,6 +35,8 @@ public class AddBookDialog extends JDialog {
 	private JButton addButton;
 	private JButton cancelButton;
 	private JSpinner copyValSpinner;
+	private JTextField editionTextField;
+	private JLabel lblEdition;
 
 	/**
 	 * Create the dialog.
@@ -46,65 +48,71 @@ public class AddBookDialog extends JDialog {
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(new MigLayout("", "[fill][grow]",
-				"[20.00][20.00][20.00][20.00][20.00][20.00,grow][20.00]"));
+		contentPanel.setLayout(new MigLayout("", "[fill][grow]", "[20.00][][20.00][20.00][20.00][20.00][20.00,grow][20.00]"));
 
 		JLabel lblTitle = new JLabel("Title:");
-		contentPanel.add(lblTitle, "cell 0 0");
+		contentPanel.add(lblTitle, "cell 0 0,alignx left");
 
 		titleTextField = new JTextField();
 		titleTextField.setName("titleTextField");
 		contentPanel.add(titleTextField, "cell 1 0,growx");
 		titleTextField.setColumns(10);
+		
+		lblEdition = new JLabel("Edition:");
+		contentPanel.add(lblEdition, "cell 0 1,alignx left");
+		
+		editionTextField = new JTextField();
+		contentPanel.add(editionTextField, "cell 1 1,growx");
+		editionTextField.setColumns(10);
 
 		JLabel lblAuthor = new JLabel("Author:");
-		contentPanel.add(lblAuthor, "cell 0 1");
+		contentPanel.add(lblAuthor, "cell 0 2,alignx left");
 
 		authorTextField = new JTextField();
 		authorTextField.setName("authorTextField");
-		contentPanel.add(authorTextField, "cell 1 1,growx");
+		contentPanel.add(authorTextField, "cell 1 2,growx");
 		authorTextField.setColumns(10);
 
 		JLabel lblYearPublish = new JLabel("Year Published:");
-		contentPanel.add(lblYearPublish, "cell 0 2");
+		contentPanel.add(lblYearPublish, "cell 0 3,alignx left");
 
 		yearPublishTextField = new JTextField();
 		yearPublishTextField.setName("yearPublishTextField");
-		contentPanel.add(yearPublishTextField, "cell 1 2,growx");
+		contentPanel.add(yearPublishTextField, "cell 1 3,growx");
 		yearPublishTextField.setColumns(10);
 
 		JLabel lblPublisher = new JLabel("Publisher:");
-		contentPanel.add(lblPublisher, "cell 0 3");
+		contentPanel.add(lblPublisher, "cell 0 4,alignx left");
 
 		publisherTextField = new JTextField();
 		publisherTextField.setName("publisherTextField");
-		contentPanel.add(publisherTextField, "cell 1 3,growx");
+		contentPanel.add(publisherTextField, "cell 1 4,growx");
 		publisherTextField.setColumns(10);
 
 		JLabel lblIsbn = new JLabel("ISBN:");
-		contentPanel.add(lblIsbn, "cell 0 4");
+		contentPanel.add(lblIsbn, "cell 0 5,alignx left");
 
 		isbnTextField = new JTextField();
 		isbnTextField.setName("isbnTextField");
-		contentPanel.add(isbnTextField, "cell 1 4,growx");
+		contentPanel.add(isbnTextField, "cell 1 5,growx");
 		isbnTextField.setColumns(10);
 
 		JLabel lblDescription = new JLabel("Description:");
-		contentPanel.add(lblDescription, "cell 0 5,aligny top");
+		contentPanel.add(lblDescription, "cell 0 6,alignx left,aligny top");
 
 		JScrollPane scrollPane = new JScrollPane();
-		contentPanel.add(scrollPane, "cell 1 5,grow");
+		contentPanel.add(scrollPane, "cell 1 6,grow");
 
 		descriptionTextArea = new JTextArea();
 		descriptionTextArea.setName("descriptionTextArea");
 		scrollPane.setViewportView(descriptionTextArea);
 
 		JLabel lblCopies = new JLabel("Copies:");
-		contentPanel.add(lblCopies, "cell 0 6");
+		contentPanel.add(lblCopies, "cell 0 7,alignx left");
 
 		copyValSpinner = new JSpinner();
 		copyValSpinner.setModel(new SpinnerNumberModel(1, 0, 2147483647, 1));
-		contentPanel.add(copyValSpinner, "cell 1 6,alignx left");
+		contentPanel.add(copyValSpinner, "cell 1 7,alignx left");
 
 		JPanel buttonPane = new JPanel();
 		getContentPane().add(buttonPane, BorderLayout.SOUTH);
@@ -173,6 +181,7 @@ public class AddBookDialog extends JDialog {
 		newBook.setPublisher(publisherTextField.getText());
 		newBook.setTitle(titleTextField.getText());
 		newBook.setYearPublish(Integer.parseInt(yearPublishTextField.getText()));
+		newBook.setEdition(editionTextField.getText());
 		return newBook;
 	}
 }
