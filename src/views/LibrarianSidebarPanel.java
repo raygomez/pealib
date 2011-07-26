6c00e1bc7a754c56c8b8a93959658c879e183eea
rayvincent.gomez <rayvincent.gomez@localhost>
2011-07-26 12:35:26 +0800

    Added MyButton class that place the icon above the text. This is to be used by LibrarianSidebarPanel and UserSidebarPanel.

--- src/views/LibrarianSidebarPanel.java
+++ src/views/LibrarianSidebarPanel.java
@@ -3,11 +3,13 @@ package views;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import net.miginfocom.swing.MigLayout;
+import utilities.MyButton;
 
 public class LibrarianSidebarPanel extends JPanel {
 
@@ -21,54 +23,58 @@ public class LibrarianSidebarPanel extends JPanel {
 	private JButton logoutButton;
 	private JButton bookTransactionsButton;
 
-	public LibrarianSidebarPanel(){
-		
-		setLayout(new MigLayout("wrap 1","[grow]","[]5[]"));
+	public LibrarianSidebarPanel() {
 
-		viewBooksButton = new JButton("Search Books");
-		viewUsersButton = new JButton("Search Users");
-		editProfileButton = new JButton("Edit Profile");
-		logoutButton = new JButton("Logout");
-		bookTransactionsButton = new JButton("Book Transactions");
-		
+		setLayout(new MigLayout("wrap 1", "[grow]", "[]5[]"));
+
+		viewBooksButton = new MyButton("Search Books", new ImageIcon(
+				"resources/images/Book.png"));
+		viewUsersButton = new MyButton("Search Users", new ImageIcon(
+				"resources/images/user32x32.png"));
+		editProfileButton = new MyButton("Edit Profile", new ImageIcon(
+				"resources/images/editProfile.png"));
+		logoutButton = new MyButton("Logout", new ImageIcon(
+				"resources/images/logout32x32.png"));
+		bookTransactionsButton = new MyButton("Book Transactions",
+				new ImageIcon("resources/images/checkInOut32x32.png"));
+
 		add(viewBooksButton, "growx");
 		add(viewUsersButton, "growx");
 		add(editProfileButton, "growx");
 		add(bookTransactionsButton, "growx");
 		add(logoutButton, "growx, wrap push");
-		
+
 	}
-	
-	public void addViewBooksListener(ActionListener viewBooks){
+
+	public void addViewBooksListener(ActionListener viewBooks) {
 		viewBooksButton.addActionListener(viewBooks);
 	}
-	
-	public void addViewUsersListener(ActionListener viewUsers){
+
+	public void addViewUsersListener(ActionListener viewUsers) {
 		viewUsersButton.addActionListener(viewUsers);
 	}
-	
-	public void addBookTransactionsListener(ActionListener bookTransactions){
+
+	public void addBookTransactionsListener(ActionListener bookTransactions) {
 		bookTransactionsButton.addActionListener(bookTransactions);
 	}
-	
-	public void addEditProfileListener(ActionListener editProfile){
+
+	public void addEditProfileListener(ActionListener editProfile) {
 		editProfileButton.addActionListener(editProfile);
 	}
-	
-	public void addLogoutListener(ActionListener logout){
+
+	public void addLogoutListener(ActionListener logout) {
 		logoutButton.addActionListener(logout);
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		
-		frame.setLayout(new MigLayout("","[][grow]","[grow]"));
-		frame.add(new LibrarianSidebarPanel(),"aligny top");
+
+		frame.setLayout(new MigLayout("", "[][grow]", "[grow]"));
+		frame.add(new LibrarianSidebarPanel(), "aligny top");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setMinimumSize(new Dimension(1024,768));
-		
+		frame.setMinimumSize(new Dimension(1024, 768));
+
 		frame.setVisible(true);
 	}
-	
-	
+
 }
