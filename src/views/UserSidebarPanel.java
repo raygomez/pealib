3c74c7dd58139ad4c78b92728f59abe501892d05
kanra.cpg <kanra.cpg@localhost>
2011-07-27 14:20:05 +0800

    Changed buttons to ToggleButton

--- src/views/UserSidebarPanel.java
+++ src/views/UserSidebarPanel.java
@@ -3,10 +3,12 @@ package views;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 
 import utilities.MyButton;
 
@@ -18,13 +20,16 @@ public class UserSidebarPanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JButton viewBooksButton;
-	private JButton editProfileButton;
-	private JButton logoutButton;
-	private JButton transactionHistoryButton;
-
+	private JToggleButton viewBooksButton;
+	private JToggleButton editProfileButton;
+	private JToggleButton logoutButton;
+	private JToggleButton transactionHistoryButton;
+	private ButtonGroup buttonGroup;
+	
 	public UserSidebarPanel() {
 
+		buttonGroup = new ButtonGroup();
+		
 		setLayout(new MigLayout("wrap 1", "[grow]", "[]5[]"));
 
 		viewBooksButton = new MyButton("Search Books", new ImageIcon(
@@ -36,11 +41,18 @@ public class UserSidebarPanel extends JPanel {
 		transactionHistoryButton = new MyButton("View E-Library Card",
 				new ImageIcon("resources/images/elib32x32.png"));
 
+		buttonGroup.add(viewBooksButton);
+		buttonGroup.add(editProfileButton);
+		buttonGroup.add(logoutButton);
+		buttonGroup.add(transactionHistoryButton);
+		
 		add(viewBooksButton, "growx");
 		add(editProfileButton, "growx");
 		add(transactionHistoryButton, "growx");
 		add(logoutButton, "growx");
 
+		viewBooksButton.getModel().setPressed(true);
+		viewBooksButton.setSelected(true);
 	}
 
 	public void addViewBooksListener(ActionListener searchBooks) {
