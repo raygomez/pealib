7c38f38151c2ae21b37472779effaf188f90ed17
rayvincent.gomez <rayvincent.gomez@localhost>
2011-07-27 12:34:50 +0800

    TransactionController now throws and handles Exception. PeaLibrary now throws and handles Exception.

--- src/pealib/PeaLibrary.java
+++ src/pealib/PeaLibrary.java
@@ -3,19 +3,17 @@ package pealib;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 import models.User;
-
+import utilities.Connector;
+import utilities.CrashHandler;
+import views.LibrarianSidebarPanel;
+import views.MainFrame;
+import views.UserSidebarPanel;
 import controllers.AuthenticationController;
 import controllers.BookController;
 import controllers.ELibController;
 import controllers.TransactionController;
 import controllers.UserController;
-
-import utilities.Connector;
-import views.LibrarianSidebarPanel;
-import views.MainFrame;
-import views.UserSidebarPanel;
 
 public class PeaLibrary {
 
@@ -25,13 +23,13 @@ public class PeaLibrary {
 	private BookController bookControl;
 	private ELibController elibControl;
 	private TransactionController transactionControl;
-	
+
 	private UserSidebarPanel userSidebarPanel;
 	private LibrarianSidebarPanel librarianSidebarPanel;
-	
+
 	private User currentUser;
 
-	public PeaLibrary(){
+	public PeaLibrary() {
 		new Connector();
 	}
 
@@ -43,7 +41,8 @@ public class PeaLibrary {
 	}
 
 	/**
-	 * @param frame the frame to set
+	 * @param frame
+	 *            the frame to set
 	 */
 	public void setFrame(MainFrame frame) {
 		this.frame = frame;
@@ -57,7 +56,8 @@ public class PeaLibrary {
 	}
 
 	/**
-	 * @param currentUser the currentUser to set
+	 * @param currentUser
+	 *            the currentUser to set
 	 */
 	public void setCurrentUser(User currentUser) {
 		this.currentUser = currentUser;
@@ -71,150 +71,156 @@ public class PeaLibrary {
 	}
 
 	/**
-	 * @param transactionControl the transactionControl to set
+	 * @param transactionControl
+	 *            the transactionControl to set
 	 */
 	public void setTransactionControl(TransactionController transactionControl) {
 		this.transactionControl = transactionControl;
 	}
-	public void authenticate(){
+
+	public void authenticate() {
 		authControl = new AuthenticationController();
 		AuthenticationController.getLogin().setVisible(true);
-		setCurrentUser(authControl.getUser());		
+		setCurrentUser(authControl.getUser());
 		initialize();
 	}
 
 	public void initialize() {
-		
+
 		setFrame(new MainFrame());
-		
-		if(getCurrentUser() == null){
+
+		if (getCurrentUser() == null) {
 			System.exit(0);
 		}
-		
+
 		initializedLoggedUser();
 	}
-	
-	private void initializedLoggedUser(){
-		//currentUser = user;
-		//currentUser = new User(101123,"jajalim","1234567","Jaja","Lim","jjlim@gmail.com","UP Ayala Technohub", "09171234567",1,"Librarian");
-		
-		getFrame().setWelcomeLabel(getCurrentUser().getFirstName()+" "+getCurrentUser().getLastName());
-		
+
+	private void initializedLoggedUser() {
+		// currentUser = user;
+		// currentUser = new
+		// User(101123,"jajalim","1234567","Jaja","Lim","jjlim@gmail.com","UP Ayala Technohub",
+		// "09171234567",1,"Librarian");
+
+		getFrame().setWelcomeLabel(
+				getCurrentUser().getFirstName() + " "
+						+ getCurrentUser().getLastName());
+
 		try {
 			bookControl = new BookController(getCurrentUser());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			CrashHandler.handle();
+
 		}
 		try {
 			userControl = new UserController(getCurrentUser());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			CrashHandler.handle();
 		}
-		
+
 		userControl.getUserInfoPanel().addSaveListener(updateCurrentUser);
-		
-		if(getCurrentUser().getType().equals("Librarian")){
+
+		if (getCurrentUser().getType().equals("Librarian")) {
 			initializeLibrarian();
-		}
-		else if(getCurrentUser().getType().equals("User")){
+		} else if (getCurrentUser().getType().equals("User")) {
 			initializeUser();
 		}
 	}
-	
-	private void initializeLibrarian(){
-		
+
+	private void initializeLibrarian() {
+
 		try {
 			setTransactionControl(new TransactionController());
 		} catch (Exception e) {
-			e.printStackTrace();
+			CrashHandler.handle();
 		}
-		
+
 		librarianSidebarPanel = new LibrarianSidebarPanel();
 		initializeSidebarPanel(librarianSidebarPanel);
-		
+
 		getFrame().setSidebarPanel(librarianSidebarPanel);
 		frame.setContentPanel(transactionControl.getTabbedPane());
 		getFrame().validate();
 		getFrame().repaint();
 	}
-	
-	private void initializeUser(){
-		
+
+	private void initializeUser() {
+
 		elibControl = new ELibController(getCurrentUser());
 		userSidebarPanel = new UserSidebarPanel();
 		initializeSidebarPanel(userSidebarPanel);
-		
+
 		getFrame().setSidebarPanel(userSidebarPanel);
 		try {
 			getFrame().setContentPanel(bookControl.getBookLayoutPanel());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			CrashHandler.handle();
+
 		}
 		getFrame().validate();
 		getFrame().repaint();
-		
+
 	}
-	
+
 	private void initializeSidebarPanel(UserSidebarPanel userSidebarPanel) {
 		userSidebarPanel.addViewBooksListener(viewBooks);
 		userSidebarPanel.addEditProfileListener(showEditProfile);
-		userSidebarPanel.addShowTransactionHistoryListener(showBookTransactions);
+		userSidebarPanel
+				.addShowTransactionHistoryListener(showBookTransactions);
 		userSidebarPanel.addLogoutListener(logout);
 	}
 
-	private void initializeSidebarPanel(LibrarianSidebarPanel librarianSidebarPanel) {
+	private void initializeSidebarPanel(
+			LibrarianSidebarPanel librarianSidebarPanel) {
 		librarianSidebarPanel.addViewBooksListener(viewBooks);
 		librarianSidebarPanel.addViewUsersListener(viewUsers);
 		librarianSidebarPanel.addEditProfileListener(showEditProfile);
 		librarianSidebarPanel.addBookTransactionsListener(showBookTransactions);
 		librarianSidebarPanel.addLogoutListener(logout);
 	}
-	
-	public MainFrame getMainFrame(){
+
+	public MainFrame getMainFrame() {
 		return getFrame();
 	}
-	
+
 	private ActionListener viewBooks = new ActionListener() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			try {
 				getFrame().setContentPanel(bookControl.getBookLayoutPanel());
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				CrashHandler.handle();
+
 			}
 		}
 	};
-	
+
 	private ActionListener viewUsers = new ActionListener() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			getFrame().setContentPanel(userControl.getLayoutPanel());
 		}
 	};
 	private ActionListener showEditProfile = new ActionListener() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 
 			userControl.getUserInfoPanel().setFields(currentUser);
-			
+
 			userControl.getUserInfoPanel().setFirstNameEnabled(false);
 			userControl.getUserInfoPanel().setLastNameEnabled(false);
-			
+
 			getFrame().setContentPanel(userControl.getUserInfoPanel());
 		}
 	};
-	
+
 	private ActionListener logout = new ActionListener() {
-		
+
 		@Override
-		public void actionPerformed(ActionEvent e) {			
+		public void actionPerformed(ActionEvent e) {
 			getFrame().setVisible(false);
 			getFrame().dispose();
 			setCurrentUser(null);
@@ -222,30 +228,29 @@ public class PeaLibrary {
 			getFrame().setVisible(true);
 		}
 	};
-	
+
 	private ActionListener showBookTransactions = new ActionListener() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			if(getCurrentUser().getType().equals("Librarian")){
+			if (getCurrentUser().getType().equals("Librarian")) {
 				frame.setContentPanel(transactionControl.getTabbedPane());
-			}
-			else if(getCurrentUser().getType().equals("User")){
+			} else if (getCurrentUser().getType().equals("User")) {
 				getFrame().setContentPanel(elibControl.getView());
 			}
 		}
 	};
-	
+
 	private ActionListener updateCurrentUser = new ActionListener() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			setCurrentUser(userControl.getCurrentUser());			
+			setCurrentUser(userControl.getCurrentUser());
 		}
 	};
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
 		PeaLibrary app = new PeaLibrary();
 		app.authenticate();
 		app.getFrame().setVisible(true);
