cbb4d709af6c3b196f60b98e72f11334b7497f9c
rayvincent.gomez <rayvincent.gomez@localhost>
2011-07-28 09:27:10 +0800

    Updated test due to change in dates.

--- test/models/TransactionDAOTest.java
+++ test/models/TransactionDAOTest.java
@@ -225,14 +225,14 @@ public class TransactionDAOTest {
 	public void testGetDaysOverdue() throws Exception {
 		User user = UserDAO.getUserById(3);
 		Book book = BookDAO.getBookById(2);
-		assertEquals(42, TransactionDAO.getDaysOverdue(book, user));
+		assertEquals(43, TransactionDAO.getDaysOverdue(book, user));
 	}
 
 	@Test
 	public void testGetDaysOverdueBorrowTransaction() throws Exception {
 		BorrowTransaction transaction = TransactionDAO
 				.getBorrowTransactionById(3);
-		assertEquals(42, TransactionDAO.getDaysOverdue(transaction));
+		assertEquals(43, TransactionDAO.getDaysOverdue(transaction));
 	}
 
 	@Test
