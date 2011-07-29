643cb71bf2ec14bc1f71bcea33e1a239dce1581d
rayvincent.gomez <rayvincent.gomez@localhost>
2011-07-29 19:49:59 +0800

    Remove unused setters in ReserveTransactionClass.

--- src/models/ReserveTransaction.java
+++ src/models/ReserveTransaction.java
@@ -17,24 +17,13 @@ public class ReserveTransaction {
 		return user;
 	}
 
-	public void setUser(User user) {
-		this.user = user;
-	}
-
 	public Book getBook() {
 		return book;
 	}
 
-	public void setBook(Book book) {
-		this.book = book;
-	}
 
 	public Date getDateReserved() {
 		return dateReserved;
-	}
-
-	public void setDateReserved(Date dateReserved) {
-		this.dateReserved = dateReserved;
 	}
 
 }
