5e3a6eec892ee7e15268857196cd6bbf352f4d09
kanra.cpg <kanra.cpg@localhost>
2011-07-29 17:09:09 +0800

    update test for usersearch ui

--- test/views/UserSearchPanelTest.java
+++ test/views/UserSearchPanelTest.java
@@ -18,18 +18,25 @@ import org.uispec4j.UISpecTestCase;
 import org.unitils.UnitilsJUnit4TestClassRunner;
 import org.unitils.dbunit.annotation.DataSet;
 
+import controllers.UserController;
+
 import utilities.Connector;
 import utilities.Constants;
 
-@DataSet({ "../models/user.xml" })
+//@DataSet({ "../models/user.xml" })
 @RunWith(UnitilsJUnit4TestClassRunner.class)
 public class UserSearchPanelTest extends UISpecTestCase {
+	UserSearchPanel userSearch;
+	UserController control;
 	Panel panel;
+	TabGroup tabGroup;
 	
 	@Before
 	public void setUp() throws Exception {
 		Connector.init(Constants.TEST_CONFIG);
-		panel = new Panel(new UserSearchPanel());
+		userSearch = new UserSearchPanel();
+		panel = new Panel(userSearch);
+		tabGroup = panel.getTabGroup();
 	}
 	
 	@Test
@@ -40,89 +47,14 @@ public class UserSearchPanelTest extends UISpecTestCase {
 		assertThat(panel.getInputTextBox().isEnabled());
 		assertThat(panel.getInputTextBox().isVisible());
 		assertEquals("",panel.getInputTextBox().getText());
-		
-		TabGroup tabGroup = panel.getTabGroup();
+	
 		assertNotNull(tabGroup);
 		assertThat(tabGroup.isEnabled());
 		assertThat(tabGroup.isVisible());
 
 		assertThat(tabGroup.tabNamesEquals(new String[] { "User Accounts", "Pending Applications" }));
 		assertThat(tabGroup.selectedTabEquals("User Accounts"));
+		
+		tabGroup.selectTab("Pending Applications");		
 	}
-/*	
-	@Test
-	public void testGetFieldSearch() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetSelectedTab() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetUsersTable() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetPendingTable() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetCbAll() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetModelPending() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetModelUsers() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testUserSearchPanel() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testUsersPanel() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPendingAppPanel() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetTableModel() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testResetTabPane() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testTogglePendingButtons() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToggleAllPendingComp() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddListeners() {
-		fail("Not yet implemented");
-	}
-*/
 }
